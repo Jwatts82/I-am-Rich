06607ff6a38056781433b0c39aab9f8b2313c8c3
jwatts82 <wattscodes@gmail.com>
2021-05-03 10:10:11 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Jessica Watts on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
